ef2c5dbd0afca49a63fb6c7c4750faeca7279bf8
reda30 <redaragab306@gmail.com>
2025-10-07 23:30:40 +0300

    adding automapper and create prfoile class

---
+++ PlatformService/Profiles/PlatformPrfoile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformPrfoile:Profile
+    {
+        public PlatformPrfoile()
+        {
+            //response 
+            CreateMap<Platform,PlatformReadDto>();
+            //request
+            CreateMap<PlatformCreateDto,Platform>();    
+        }
+    }
+}
